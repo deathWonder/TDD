176ba4afd7d004c44ec172b8d8def5c60fa50608
Sabir <bovba179@gmail.com>
2023-03-04 05:39:39 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,3 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
-   
+    private final Map<String, String> contactList  = new HashMap<>();
+    private static PhoneBook instance = null;
+    private PhoneBook(){}
+
+    public static PhoneBook getInstance() {
+        if (instance == null) instance = new PhoneBook();
+        return instance;
+    }
+    public Map<String, String> getContactList(){
+        return contactList;
+    }
+    public int add(String name, String number){
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
 public class PhoneBookTest {
-   
+    PhoneBook phoneBook = PhoneBook.getInstance();
+    @ParameterizedTest
+    @MethodSource("addSource")
+    public void testAdd(String name, String number, int expected) {
+        int result = phoneBook.add(name, number);
+        Assertions.assertEquals(expected, result);
+    }
+    public static Stream<Arguments> addSource(){
+        return Stream.of(
+                Arguments.of("Name1", "89999999990", 1),
+                Arguments.of("Name2", "89999999991", 2),
+                Arguments.of("Name3", "89999999992", 3)
+        );
+    }
 }
